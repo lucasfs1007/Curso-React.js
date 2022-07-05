903bc1034577735aa5184776305f8005147f0939
lucasfs1007 <lucasfs1007@gmail.com>
2022-07-05 09:33:34 -0300

    add my first activity

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!!!</h1>
+      <h2>Emmet funcionou</h2>
+      <div>
+        <h1>Meu primeiro desafio deste curso</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
